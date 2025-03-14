2fb1860560eb62d5806624a4e847656452fcbf2b
Colm Nolan_B00152001 <B00152001@mytudublin.ie>
2025-03-14 01:16:58 +0000

    Add files via upload

---
+++ Project_Website/signup.php
@@ -0,0 +1,27 @@
+
+<?php include 'templates/header.php'; ?> 
+
+<?php
+
+
+$conn = new mysqli("localhost", "root", "", "los_boyos_hermanos");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $conn->query("INSERT INTO users (email, password) VALUES ('$email', '$password')");
+    echo "Registered successfully!";
+}
+
+$conn->close();
+?>
+
+<form method="POST">
+    <br>
+    Email: <input type="email" name="email" required><br><br>
+ Password: <input type="password" name="password" required><br><br>
+    <button type="submit">Register</button><br><br>
+</form>
+
+<?php include 'templates/footer.php'; ?>
