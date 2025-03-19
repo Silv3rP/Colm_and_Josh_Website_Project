e535630be5a3c1021eec77b0a375a3ed78d51860
Colm Nolan_B00152001 <B00152001@mytudublin.ie>
2025-03-19 14:41:36 +0000

    Delete WebsiteProject directory

--- WebsiteProject/about.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-
-<h2>About Los Boyo's Hermanos</h2>
-<p>Welcome to Los Boyo's Hermanos, your go-to spot for delicious, crispy fried chicken and fast-food meals.</p>
-
-<?php include 'footer.php'; ?>
--- WebsiteProject/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>&copy; 2024 Los Boyo's Hermanos. All Rights Reserved.</p>
-</footer>
-
-</body>
-</html>
